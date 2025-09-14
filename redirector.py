2050f6325ccc71327017abd66c56936edbac3c76
Indika Viraj <sanjuvirajlive@gmail.com>
2025-09-15 00:59:51 +0530

    rewrite redirector

--- redirector.py
+++ redirector.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect
+from flask import Flask, Response
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 load_dotenv(override=True)
 app = Flask(__name__)
-mongo_client = MongoClient(os.getenv('MONGODB_URI'), server_api=ServerApi('1'))
+mongo_client = MongoClient(os.environ['MONGODB_URI'], server_api=ServerApi('1'))
 
 def getconfig(key, default=None):
     result = mongo_client.default.config.find_one({'key':key})
@@ -12,9 +12,15 @@ def getconfig(key, default=None):
         return default
     return result['value']
 
-@app.route('/')
+@app.route('/url')
+def show():
+    return f"https://t.me/{getconfig('BOT_USERNAME')}"
+
+@app.route('/lol', defaults={'anything': ''})
+@app.route('/<path:anything>')
 def redirect():
-    return redirect(f"https://t.me/{getconfig('BOT_USERNAME')}", code=301)
+    headers = {"Location": f"https://t.me/{getconfig('BOT_USERNAME')}", "Referrer-Policy": "no-referrer"}
+    return Response("Moved Permanently", headers=headers, status=301)
 
 if __name__ == "__main__":
     app.run()
